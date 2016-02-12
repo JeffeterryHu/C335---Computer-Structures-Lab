acd48c8e145c127f4637d7c4acb102ecd72d9447
hushi <hushi@indiana.edu>
2016-02-11 20:00:59 -0500

    editing uart.h & uart.c

--- driver/src/f3d_uart.c
+++ driver/src/f3d_uart.c
@@ -9,17 +9,51 @@
 #include <f3d_uart.h>
 //the initialization function to call
 void f3d_uart_init(void) {
-
+  /* Initialize the clocks */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
   
+
+  /* Initialize the RX and TX Pins */
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC,&GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOC,4,GPIO_AF_7);
+  GPIO_PinAFConfig(GPIOC,5,GPIO_AF_7);
+  
+  /* UART Initialization */
+  USART_InitTypeDef USART_InitStructure;
+  USART_StructInit(&USART_InitStructure);
+  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_Init(USART1 ,&USART_InitStructure);
+  USART_Cmd(USART1 , ENABLE);
+
 }
+
 //sends a character
 int putchar(int c) {
+  while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == (uint16_t)RESET);
+  USART_SendData(USART1, c);
+  return 0;
 } 
+
 //gets a character
 int getchar(void) {
+  while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == (uint16_t)RESET);
+  return USART_ReceiveData(USART1);
 }
+
 //sends a string
 void putstring(char *s) {
+  int i = 0;
+  for( i; s[i] != '\0'; i++ ) { putchar(s[i]); }
 }
 
 
---
+++ driver/src/f3d_uart.h
@@ -0,0 +1,2 @@
+int putchar(int);
+int getchar(void);
