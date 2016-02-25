4c2f2fb1fd4f05264ef26e448686b182d083dcc8
hushi <hushi@indiana.edu>
2016-02-25 16:57:35 -0500

    upload

--- lab6/main.c
+++ lab6/main.c
@@ -19,11 +19,40 @@
 
 // delay function
 void delay(void){
-	int i = 1800000;
+	int i = 1200000;
 	while(i-- > 0){
 		asm("nop");
 	}
 }
+
+/*
+void choice(int i){
+	char ch;
+  	scanf("%c",&ch);
+ 	if(ch == 'x'){
+    		i= 1;
+  	} else if (ch == 'y'){
+    		i= 2;
+  	} else if (ch == 'z'){
+    		i= 3;
+  	} else {
+    		i = 0;
+	}
+}
+*/
+
+/*
+int dpg;
+
+void LightLED(int dpg){
+	int min = 0;
+	int max = 0;
+	int step = 500 / 5;
+	
+	f3d_led_on(9);
+	
+}
+*/
 
 // main function
 int main(void){
@@ -47,8 +76,8 @@ int main(void){
   setvbuf(stderr, NULL, _IONBF, 0);
 
   printf("Angular Velocity Visualization Application\n");
-  printf("Press user button will start the app and change the axises showing.\n"); 
-  
+  printf("Press user button will start the app and change the axises showing.\n");  
+
   while(1){
 
   	button_state = user_btn_read();
@@ -59,11 +88,52 @@ int main(void){
 		delay();
 	}
 	
-	while(example%4 == 1) {
+
+	//char ch;
+	//ch = getchar();
+	//char ch;
+	//scanf("%c",&ch);
+
+	while(example%4 == 1 /*|| ch == 'x'*/) {
 		f3d_gyro_getdata(Data); 
 	      	printf("X axis: %f\n", Data[0]);
 	      	delay();
-		     
+
+		
+		if(Data[0] > 0 && Data[0] < 10){
+			f3d_led_on(0);
+		}else{
+			f3d_led_off(0);
+		}
+		
+		if(Data[0] > 10 && Data[0] < 20){
+			f3d_led_on(7);
+			//f3d_led_on(6);
+			//f3d_led_on(7);
+		}
+
+		if(Data[0] > 20 && Data[0] < 30){
+                        //f3d_led_on(4);
+			//f3d_led_on(5);
+			f3d_led_on(6);
+			//f3d_led_on(7);
+			//f3d_led_on(0);
+                }else{
+			f3d_led_all_off();
+		}
+
+
+		/*
+		if(ch == 'x'){}
+		else if(ch == 'y'){
+			example--;
+		}
+		else if(ch == 'z'){
+			example -= 2;
+		}
+		else{}
+		*/		
+
 		if (user_btn_read()) {
 			example++;
 			//delay();
@@ -71,11 +141,23 @@ int main(void){
 		}	
 	}
 
-	while(example%4 == 2){
+	while(example%4 == 2 /*|| ch == 'y'*/){
 		f3d_gyro_getdata(Data);
 		printf("Y axis: %f\n", Data[1]);
 		delay();
 		
+		/*
+                if(ch == 'x'){
+                	example--;
+                }
+                else if(ch == 'y'){
+                }
+                else if(ch == 'z'){
+                	example++;
+                }
+                else{}
+		*/
+
 		if(user_btn_read()){
 			example++;
 			//delay();
@@ -83,11 +165,23 @@ int main(void){
 		}
 	}
 
-	while(example%4 == 3){
+	while(example%4 == 3 /*|| ch == 'z'*/){
 		f3d_gyro_getdata(Data);
 		printf("Z axis: %f\n", Data[2]);
 		delay();
-		
+	
+		/*
+                if(ch == 'x'){
+                        example -= 2;
+                }
+                else if(ch == 'y'){
+			example += 3;
+                }
+                else if(ch == 'z'){
+                }
+                else{}
+		*/
+	
 		if(user_btn_read()){
 			example++;
 			//delay();
