9fc1dd06b0b6e8df92fbc019dbd2b63a683fc2e4
hushi <hushi@indiana.edu>
2016-02-24 21:32:58 -0500

    upload debug folder

--- debug/main.c
+++ debug/main.c
@@ -17,37 +17,37 @@
 
 int gitBitsChar(unsigned char c, char answer[]){
   int i=7;
-  while(i>-1){
+  while(i>=0){
     if(c&1) answer[i] = '1';
     else answer[i] = '0';
     c=c>>1;
     i--;
   }
-  answer[7]=0;
+  answer[8]=0;
   return 0;
 }
 
 int gitBitsShort(unsigned short s, char answer[]){
-  int i = 16;
-  while(s<16){
+  int i = 15;
+  while(s>=0){
     if(s&1) answer[i] = '1';
     else answer[i] = '0';
-    s=s<<1;
-    i++;
+    s=s>>1;
+    i--;
   }
-  answer[i]=0;
+  answer[16]=0;
   return 0;
 }
 
 int gitBitsInt(int yikes, char answer[]){
-  int i = 0;
-  while(yikes<32){
+  int i = 31;
+  while(i>=0){
     if(yikes&1) answer[i] = '1';
     else answer[i] = '0';
-    yikes=yikes<<1;
-    i++;
+    yikes=yikes>>1;
+    i--;
   }
-  answer[i]=0;
+  answer[32]=0;
   return 0;
 }
 
