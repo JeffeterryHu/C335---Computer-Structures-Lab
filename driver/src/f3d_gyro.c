b3e20a29bdcb3a6d5b5608de712cba364961a5eb
hushi <hushi@indiana.edu>
2016-02-24 21:33:37 -0500

    edit driver file

--- driver/src/f3d_gyro.c
+++ driver/src/f3d_gyro.c
@@ -1,28 +1,33 @@
 #include <f3d_gyro.h>
 #include <stm32f30x.h>
 void f3d_gyro_interface_init() {
+  
   GPIO_InitTypeDef GPIO_InitStructure;
+  SPI_InitTypeDef SPI_InitStructure;
+  // Pin initialization
   GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOE, &GPIO_InitStructure);
 
+  // Initialization for the following:
   //SCK PA5 
   GPIO_PinAFConfig(GPIOC,5,GPIO_AF_5);
   //MISO PA6
   GPIO_PinAFConfig(GPIOC,6,GPIO_AF_5);
   //MOSI PA7
   GPIO_PinAFConfig(GPIOC,7,GPIO_AF_5);
-  //CS PE3
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  //set the CS high
-  GYRO_CS_HIGH();
+
    
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); // SCK, MOSI, MISO on PORTA
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE); // CS on port E
+
   //SPI Initialization and configuration
-  SPI_InitTypeDef SPI_InitStructure;
+  SPI_I2S_DeInit(SPI1);
   SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
   SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
   SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
@@ -35,12 +40,25 @@ void f3d_gyro_interface_init() {
   SPI_Init(SPI1, &SPI_InitStructure);
   SPI_RxFIFOThresholdConfig(SPI1, SPI_RxFIFOThreshold_QF);
   SPI_Cmd(SPI1, ENABLE);
+
+  // CS PE3
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+  // set the CS high
+  GPIO_SetBits(GPIOE,GPIO_Pin_3);
 }
 
 //the init function to be called in your main.c
 void f3d_gyro_init(void) {
+  
   //
-  //SETTING THE CONTROL REGISTERS 
+  //SETTING THE CONTROL REGISTERS
+  
   f3d_gyro_interface_init();
   // CTRL1 Register 
   // Bit 7:6 Data Rate: Datarate 0
@@ -66,8 +84,11 @@ void f3d_gyro_init(void) {
   f3d_gyro_write(&ctrl4, 0x23, 1);
 
 }
-//to read from it
+
+  //to read from it
+
 void f3d_gyro_read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead) {
+  
   //are we reading one byte or more than one byte???
   if (NumByteToRead > 1) {
     ReadAddr |= (uint8_t)(0x80 | 0x40); // sets to multibyte mode
@@ -75,10 +96,13 @@ void f3d_gyro_read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead) {
   else {
     ReadAddr |= (uint8_t) (0x80); // sets to read mode (first bit 1)
   }
+  
   //setting chip select to low (LETS TALK!)
   GYRO_CS_LOW();
+  
   //sending address byte
   f3d_gyro_sendbyte(ReadAddr);  
+  
   while(NumByteToRead > 0x00) {
     //WE are now sending dummy data so we can read the valuable!
     //remember we must write to read!
@@ -91,15 +115,19 @@ void f3d_gyro_read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead) {
   GYRO_CS_HIGH();//setting chip select to high (DONE TALKING)
 }
 
-/*writing function*/
+  /*writing function*/
 void f3d_gyro_write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite) {
-	if (NumByteToWrite > 1){
+	
+	if (NumByteToWrite > 0x01)
+	{
 		WriteAddr |= (uint8_t) (0x40);
 	}
 	GYRO_CS_LOW();
+
 	f3d_gyro_sendbyte(WriteAddr);
-	while(NumByteToWrite > 0x00){
-		*pBuffer = f3d_gyro_sendbyte(((uint8_t)0x00));
+
+	while(NumByteToWrite >= 0x01){
+		f3d_gyro_sendbyte((*pBuffer);
     		NumByteToWrite--;
     		pBuffer++;
 	}
@@ -107,7 +135,8 @@ void f3d_gyro_write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite
 }
 
 /*sends the bytes*/
-static uint8_t f3d_gyro_sendbyte(uint8_t byte) {
+static uint8_t f3d_gyro_sendbyte(uint8_t byte)
+{
 	while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET); 
 	SPI_SendData8(SPI1, byte);
 	while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
