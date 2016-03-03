c4fe60259b6e3782aaacd64a0f0ba448c3fdf70a
hushi <hushi@indiana.edu>
2016-03-03 16:25:56 -0500

    lab7 debug

---
+++ debug_printf/main.c
@@ -0,0 +1,79 @@
+/*
+
+Nathan Krummen
+This loops through like 400 printfs per cycle, resulting in probably a 1/3rd of a second delay between lights on/lights off. Printf takes a decent amount of time to process being the point of this assignment me thinks.
+
+ */
+#include <f3d_uart.h>
+#include <stdio.h>
+#include <f3d_gyro.h>
+#include <f3d_led.h>
+#include <f3d_user_btn.h>
+#include <stdlib.h>
+
+void inits(void) {
+  f3d_led_init();
+  f3d_user_btn_init();
+}
+int i = 0;
+
+void main(void) {
+  inits();  
+  while(1) {
+    f3d_led_all_on();
+    int k;
+    for(k = 0; k < 100; k++) {
+    printf("abc");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    }
+    f3d_led_all_off();
+    int kk;
+    for(kk = 0; kk < 100; kk++) {
+    printf("cba");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+    printf("asdfdslf;j;slkfj;as;jfalsjfksladjfsldkf");
+  }
+  }
+
+}
+
+
+void assert_failed(uint8_t* file, uint32_t line) {
+/* Infinite loop */
+/* Use GDB to find out why we're here */
+
+  printf("Error - Check Assert_failed");
+  while (1);
+}
